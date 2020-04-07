cc2eb53bd042f537918eefd5bd68f1727478a0e0
mariapichardo <maria_pichardo@yahoo.com>
2020-04-07 12:32:57 -0400

    pass it to allison

---
+++ bubblesort.js
@@ -0,0 +1,32 @@
+function bubbleSort(arr) {
+
+
+	let swapNum = 0
+	let anotherSwap = true
+
+	while (anotherSwap) {
+		anotherSwap = false
+		for (let i = 0; i < arr.length - swapNum; i++){
+
+			if (arr[i] > arr[i + 1]) {
+				swapFun(arr, i, i + 1)
+				
+			}
+			swapNum++
+			anotherSwap = true
+
+		}
+	}
+
+	return arr
+	
+}
+
+function swapFun(arr, idx1, idx2) {
+	let temp = arr[idx1];
+	arr[idx1] = arr[idx2];
+	arr[idx2] = temp;
+	return arr;
+
+}
+
---
+++ bubblesort.spec.js
@@ -0,0 +1,13 @@
+describe('Bubble Sort', function () {
+	arrayA = [8,9,5,1,2,3,7]
+
+	it('handles an empty array', function () {
+		expect(bubbleSort([])).toEqual([]);
+	});
+	it('It returns an array', function () {
+		expect(bubbleSort([]).isArray).toEqual(true);
+	});
+	it('It returns a sorted array', function () {
+		expect(bubbleSort(arrayA)).toEqual([1,2,3,5,7,8,9]);
+	});
+});
